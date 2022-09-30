d371f072cd517e8c17f8a367d2e894478913c804
FrankTII <frank.training2@gmail.com>
2022-09-30 03:13:05 -0500

    Reconfig dbconnection in app.js

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
-const { Sequelize } = require('sequelize');
+// const { Sequelize } = require('sequelize');
 
-const dbconnect = require('./env.js');
+// const dbconnect = require('./env.js');
 
-const sequelize= new Sequelize(
-    dbconnect.dbname,
-    dbconnect.username,
-    dbconnect.password,
-        {
-        host:dbconnect.hostname,
-        dialect:'postgres',
-        native:true,
-        ssl:true
-        }
-);
+// const sequelize= new Sequelize(
+//     dbconnect.dbname,
+//     dbconnect.username,
+//     dbconnect.password,
+//         {
+//         host:dbconnect.hostname,
+//         dialect:'postgres',
+//         native:true,
+//         ssl:true
+//         }
+// );
+
+const express = require('express');
+const sequelize = require('./config/db');
+
+const app = express();
+app.use(express.json());
 
 try {
     sequelize.authenticate();
-    console.log('La conexion fue exitosa');
-  } catch (error) {
-    console.error('Hubo un problema con la conexión', error);
-  }
+    sequelize.sync();
+    console.log('Connected to DB');
+} catch (error) {
+    console.log('Unable to connect to DB:', error);
+}
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log("Server listing on PORT 3000");
+});
