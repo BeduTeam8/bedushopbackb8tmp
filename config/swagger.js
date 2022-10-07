7d12a823a4f7d5a5bdb0c298236a92dbfa49abda
Miguel Chavez <toonchavez8@gmail.com>
2022-10-07 17:26:30 -0500

    begining to work on swagger documentation

---
+++ config/swagger.js
@@ -0,0 +1,25 @@
+// swagger definition
+const swaggerOptions = {
+	definition: {
+		info: {
+			title: "BeduShop API",
+			description: "Welcome to BEDU Shop Back Team B8 API! test",
+			servers: ["http://localhost:3000/v1"],
+		},
+		openapi: "3.0.3",
+		components: {
+			securitySchemes: {
+				bearerAuth: {
+					type: "http",
+					scheme: "bearer",
+					bearerFormat: "JWT",
+					value: "Bearer <JWT token here>",
+				},
+			},
+		},
+	},
+	apis: ["./routes/*.js"],
+};
+
+// export swaggerOptions
+module.exports = swaggerOptions;
--- routes/users.js
+++ routes/users.js
@@ -11,12 +11,136 @@ const {
 	signUp,
 } = require("../controllers/users");
 
+// swagger documentation
+/**
+ * @swagger
+ * /v1/users:
+ *  get:
+ *   tags:
+ *    - Users
+ *   summary: Users list
+ *   description: get the list of users
+ *   responses:
+ *    200:
+ *     description: a welcome message
+ *    401:
+ *     description: unauthorized you need to be an admin
+ *    403:
+ *     description: forbidden
+ *   security:
+ *    - bearerAuth: []
+ */
 router.get("/", auth.admin, getUsers);
+/**
+ * @swagger
+ * /v1/users/{id}:
+ *  get:
+ *   tags:
+ *    - Users
+ *   summary: Get user by id
+ *   description: get the user by id
+ *   responses:
+ *    200:
+ *     description: ok
+ *    401:
+ *     description: unauthorized you need to be an admin
+ *    403:
+ *     description: forbidden
+ *   security:
+ *    - bearerAuth: []
+ */
 router.get("/:id", auth.admin, getUser);
 // router.post("/", createUser);
+
+/**
+ * @swagger
+ * /v1/users/{id}:
+ *  patch:
+ *   tags:
+ *    - Users
+ *   summary: Update user by id
+ *   description: update the user by id only if you are the admin or the user
+ *   requestBody:
+ *    required: true
+ *    content:
+ *     application/json:
+ *	    schema:
+ *   responses:
+ *    200:
+ *     description: ok
+ *    401:
+ *     description: unauthorized you need to be an admin
+ *    403:
+ *     description: forbidden
+ *   security:
+ *    - bearerAuth: []
+ */
 router.patch("/:id", auth.Userid, updateUser);
+/**
+ * @swagger
+ * /v1/users/{id}:
+ *  delete:
+ *   tags:
+ *    - Users
+ *   summary: Delete user by id
+ *   description: delete the user by id only if you are the admin or the user
+ *   responses:
+ *    200:
+ *     description: ok
+ *    401:
+ *     description: unauthorized you need to be an admin
+ *    403:
+ *     description: forbidden
+ *   security:
+ *    - bearerAuth: []
+ */
 router.delete("/:id", auth.admin, deleteUser);
+
+/**
+ * @swagger
+ * /v1/logIn:
+ *  post:
+ *   tags:
+ *    - Users
+ *   summary: Update user by id
+ *   description: update the user by id only if you are the admin or the user
+ *   responses:
+ *    200:
+ *     description: ok
+ *    401:
+ *     description: unauthorized you need to be an admin
+ *    403:
+ *     description: forbidden
+ *   security:
+ *    - bearerAuth: []
+ */
 router.post("/logIn", logIn);
+
+/**
+ * @swagger
+ * /v1/users/signUp:
+ *   put:
+ *     tags:
+ *      - Users
+ *     summary: sign up
+ *     description: sign up a new user
+ *     requestBody:
+ *       description: user object that will be created and added to the database
+ *       required: true
+ *       content:
+ *         application/json:
+ *	         schema:
+ *             $ref: '../models/users.js#/user'
+ *     responses:
+ *       200:
+ *         description: ok
+ *       401:
+ *         description: unauthorized you need to be an admin
+ *       403:
+ *         description: forbidden
+ *     security:
+ *       - bearerAuth: []
+ */
 router.post("/signUp", signUp);
 
 module.exports = router;
