6476160fa0c6ee3fffae6452c982592cff719bc8
FrankTII <frank.training2@gmail.com>
2022-10-09 16:03:18 -0500

    update app.js

--- app.js
+++ app.js
@@ -31,20 +31,9 @@ app.listen(process.env.PORT||3000, () => {
 try {
 	sequelize.authenticate();
 	sequelize.sync();
+	//RESET DB remove on PROD
 	//sequelize.sync({force:true});//RESET DB remove on PROD
 	console.log("Connected to DB");
 } catch (error) {
 	console.log("Unable to connect to DB:", error);
 }
-
-/*Function to test JOIN tables from sequelize
-async function getUserTypeAuth(id){
-const userTypeAuth = await User.findByPk(1, {
-	include: UserType,
-  });
-
-  return userTypeAuth;
-}
-  
-console.log("UserTypeAuth: ",getUserTypeAuth);
-*/
